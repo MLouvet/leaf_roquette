ee8e36c26a49dbadbfe854698ce3eecb0fe6848a
Plocate <pierre-antoine.locatelli@u-psud.fr>
2018-01-12 19:27:05 +0100

    Apparemment il manquait des trucs ?

---
+++ Leaf/Leaf.Web/ViewModel/ProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Leaf.DAL.ScaffoldedModels;
+using System.Collections.Generic;
+
+namespace Leaf
+{
+    public class ProfileViewModel
+    {
+        public List<Notification> notifications;
+        public List<Tache> taches;
+    }
+}
