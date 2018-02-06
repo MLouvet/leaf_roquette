4099c24a90aff684094bc71965983c74d1a3773b
Tlospock <meunierpaul@free.fr>
2018-02-06 20:59:32 +0100

    wip listproject - add clientView & services

---
+++ Leaf/Leaf.Web/Controllers/ClientController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Leaf.DAL.ScaffoldedModels;
+using Leaf.DAL.Services;
+using Leaf.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Leaf.Web.Controllers
+{
+    public class ClientController : Controller
+    {
+        private static ClientService _clientService;
+        private static CollaborateursService _collaborateurService;
+
+        public ClientController(LeafContext context)
+        {
+            Dal.SetBDD(context);
+            _clientService = new ClientService(context);
+            _collaborateurService = new CollaborateursService(context);
+        }
+
+        public IActionResult Profile()
+        {
+
+            var collaborateur = _collaborateurService.GetById(2);
+            var model = new ClientViewModel
+            {
+                clients = _clientService.GetByCollaborateur(collaborateur)
+            };
+
+            return View(model);
+        }
+    }
+}
--- Leaf/Leaf.Web/ViewModel/ClientViewModel.cs
+++ Leaf/Leaf.Web/ViewModel/ClientViewModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Leaf.DAL.DTO;
+
 namespace Leaf
 {
-    public class ProjectViewModel
+    public class ClientViewModel
     {
-        public List<Projet> projets;
+        public List<DAL.DTO.Client> clients;
     }
 }
