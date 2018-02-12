48b0930cfcd0dfa9a2bc25396bfa0d0fd408eb8b
Plocate <pierre-antoine.locatelli@u-psud.fr>
2018-02-12 12:52:49 +0100

    Lien vers un clien fait.

--- Leaf/Leaf.Web/Controllers/ClientsController.cs
+++ Leaf/Leaf.Web/Controllers/ClientsController.cs
@@ -2,37 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Leaf.DAL.ScaffoldedModels;
-using Leaf.DAL.Services;
+using Microsoft.AspNetCore.Mvc;
 using Leaf.Web.Models;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
+using Leaf.DAL.ScaffoldedModels;
+using Leaf.DAL.Services;
 
 namespace Leaf.Web.Controllers
 {
     public class ClientsController : Controller
     {
         private static ClientService _clientService;
-        private static CollaborateursService _collaborateurService;
 
         public ClientsController(LeafContext context)
         {
             Dal.SetBDD(context);
             _clientService = new ClientService(context);
-            _collaborateurService = new CollaborateursService(context);
         }
 
-        public IActionResult Index()
+        public IActionResult Client(int? id)
         {
-
-            var collaborateur = _collaborateurService.GetById(2);
-            var model = new ClientListViewModel
+            if(id.HasValue)
             {
-                clients = _clientService.GetByCollaborateur(collaborateur)
-            };
+                var model = new ClientViewModel
+                {
+                    client = _clientService.GetById((int)id)
+                };
+                return View(model);
+            }
 
-            return View(model);
+            return View("Error");
         }
     }
 }
